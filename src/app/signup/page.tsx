5f696d43bb21203ad4534dc9ed2da17da35e5815
Darshal Dalal <darshal.dalal.ordex@gmail.com>
2024-03-05 17:30:42 +0530

    Removed Unused Import

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
